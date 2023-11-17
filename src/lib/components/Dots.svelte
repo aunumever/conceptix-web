a4e38c3d746a344b3082cacbb788a288a4e1492b
Aunum Cadeau <aunumcadeau@gmail.com>
2023-11-17 15:46:35 -0500

    Finishing touches

---
+++ src/lib/components/Dots.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { onMount } from 'svelte'
+
+  onMount(() => {
+    VANTA.DOTS({
+      el: "#dots",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: '#2dd4bf',
+      color2: '#a5f3fc',
+      backgroundAlpha: 0,
+      size: 1.10,
+      showLines: false
+    })
+  })
+</script>
+
+<div id="dots" class="absolute top-0 left-0 right-0 h-screen w-full z-10"></div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,54 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
+  import { fade, fly } from 'svelte/transition'
+  import { onMount } from 'svelte'
+  import { expoOut } from 'svelte/easing';
+  import Dots from '$lib/components/Dots.svelte'
 
-<div class="container h-screen mx-auto flex flex-col justify-center">
-	
-    <div class="flex gap-x-3 items-center">
-      <img class="w-16" src="/brand.svg" alt="Conceptix Inc.">
-		  <h1 class="h1"><span class="gradient-heading">conceptix</span></h1>
-    </div>
-
-    <div class="space-y-5 pt-12 max-w-2xl">
-
-    <h3 class="h3">Empowering Change Through Innovative Tech</h3>
-
-    <p>At Conceptix Inc., we specialize in crafting enterprise-grade iOS and Android apps. We partner with non-profits and mission-driven organizations, turning their visions into digital realities.</p>
-
-    <h3 class="h3">Driven by Purpose: Your Mission, Our Passion.</h3>
-    
-    <p>We believe in the power of technology to make a meaningful difference. We’re more than developers; we’re your strategic partners in creating software that matters.</p>
-
-    <!-- <h1 class="h1">Clients</h1>
-
-    <p>Sustainable Gardens: Revolutionizing education through interactive hydroponic gardening apps, helping students learn from seed to harvest within their classrooms.</p>
-
-    <h1 class="h1">The Niagara Jr. Ice Dogs</h1>
-
-    <p>The Niagara Jr. Ice Dogs: Enhancing the hockey experience for special needs players with a custom app designed for easy equipment management and team communication.</p> -->
-
-    <p class="pt-8">Let's Innovate Together.</p>
-
-    <p>
-      Conceptix Inc.<br />
-      (705) 622-3107
-      </p>
-  </div>
+  let show = false
   
-    
-		
+  onMount(() => {
+    show = true
+  })
+</script>
 
+{#if show}
+
+<div in:fade={{duration: 1000}}>
+  <Dots />
+
+
+  <div in:fly={{delay: 400, duration: 1200, y: 100, easing: expoOut}} class="container p-12 h-screen flex flex-col justify-center items-center">
+    
+      <div class="flex gap-x-3 max-w-2xl w-full items-center">
+        <img class="w-16" src="/brand.svg" alt="Conceptix Inc.">
+        <h1 class="h1 sm:h2"><span class="gradient-heading">conceptix</span></h1>
+      </div>
+
+      <div class="space-y-5 pt-12 max-w-2xl">
+
+        <h3 class="h3">Empowering Change Through Innovative Tech</h3>
+
+        <p>At Conceptix Inc., we specialize in crafting enterprise-grade iOS and Android apps. We partner with non-profits and mission-driven organizations, turning their visions into digital realities.</p>
+
+        <h3 class="h3">Driven by Purpose: Your Mission, Our Passion.</h3>
+        
+        <p>We believe in the power of technology to make a meaningful difference. We’re more than developers; we’re your strategic partners in creating software that matters.</p>
+
+        <!-- <h1 class="h1">Clients</h1>
+
+        <p>Sustainable Gardens: Revolutionizing education through interactive hydroponic gardening apps, helping students learn from seed to harvest within their classrooms.</p>
+
+        <h1 class="h1">The Niagara Jr. Ice Dogs</h1>
+
+        <p>The Niagara Jr. Ice Dogs: Enhancing the hockey experience for special needs players with a custom app designed for easy equipment management and team communication.</p> -->
+
+        <p class="pt-8">Let's Innovate Together,</p>
+
+        <p>
+          Conceptix Inc.<br />
+          <span class="text-secondary-300">(705) 622-3107</span>
+        </p>
+    </div>
+  </div>
 </div>
+{/if}
